615beb179fa7730391732578e5b9e0d4b7eab640
David Südholt <david.suedholt@gmail.com>
2014-02-17 22:15:31 +0100

    First working version

---
+++ config.py
@@ -0,0 +1,13 @@
+config = {
+	#update those values accordingly
+
+	#account data for the mod account
+    'USERNAME': '',
+    'PASSWORD': '',
+    
+    #the account that posts the rounds
+    'GAMEACCOUNT': '',
+
+  	#the game subreddit
+    'SUBREDDIT': ''
+}
